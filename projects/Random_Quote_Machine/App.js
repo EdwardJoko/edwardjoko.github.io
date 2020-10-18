66b0ff6619a0cee143d81cb61777fe5657d76d84
EdwardJoko <edwardjptatuhas@outlook.com>
2020-10-19 03:10:32 +0700

    Adding async in fetching the API

--- projects/Random_Quote_Machine/App.js
+++ projects/Random_Quote_Machine/App.js
@@ -21,8 +21,8 @@ class RandomMachine extends React.Component {
 
   // Get the collection of quotes so we just need to fetch it
   // once and also set the first quote
-  getArrayOfQuote() {
-    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+  async getArrayOfQuote() {
+    await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
       .then(res => res.json())
       .then(data => {
         let quoteArray = data.quotes;
